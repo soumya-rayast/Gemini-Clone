0e58007c24499ae590049746f9db39a21478b1dd
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-05-21 16:12:59 +0530

    Gemini UI design completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/main/Main'
+
+function App() {
+  return (
+    <>
+     <Sidebar/>
+     <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import "./Main.css"
+import { assets } from '../../assets/assets'
+const Main = () => {
+  return (
+    <div className='main'>
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+            <p><span>Hello, Dev.</span></p>
+            <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+            <div className="card">
+                <p>Suggests some best movies to watch</p>
+                <img src={assets.compass_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Suggests some best place to visit in India</p>
+                <img src={assets.bulb_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Suggests some best foods to eat in Varanasi</p>
+                <img src={assets.message_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Suggests some best foods to eat in Goa</p>
+                <img src={assets.code_icon} alt="" />
+            </div>
+        </div>
+        <div className="main-bottom">
+            <div className="search-box">
+                <input type="text" placeholder='Enter a prompt here' />
+                <div>
+                    <img src={assets.gallery_icon} alt="" />
+                    <img src={assets.mic_icon} alt="" />
+                    <img src={assets.send_icon} alt="" />
+                </div>
+            </div>
+            <p className='bottom-info'>
+                Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps.
+            </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Main
