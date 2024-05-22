1f39800bb7e8d85b6fe8ce46118e2a1d97598768
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-05-23 00:04:44 +0530

    Sidebar Functionality added

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,37 +1,47 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import "./Sidebar.css"
 import { assets } from "../../assets/assets"
+import { Context } from '../../context/Context'
 const Sidebar = () => {
     // for menu 
     const [extended, setExtended] = useState(false);
+    const { onSent, prevPrompt, setRecentPrompt,newChat } = useContext(Context);
+    const loadPrompt = async (prompt) =>{
+        setRecentPrompt(prompt);
+        await onSent(prompt);
+    }
     return (
         <div className='sidebar'>
             <div className="top">
-                <img src={assets.menu_icon} alt="" className='menu' onClick={()=>setExtended(prev=>!prev)} />
-                <div className="new-chat">
+                <img src={assets.menu_icon} alt="" className='menu' onClick={() => setExtended(prev => !prev)} />
+                <div  onClick={()=>newChat()} className="new-chat">
                     <img src={assets.plus_icon} alt="" />
-                    {extended ? <p>New Chat</p> : null}
+                    {extended ? <p>New Chat</p> : null}  
                 </div>
                 {extended ? <div className="recent">
                     <p className='recent-title'>Recent</p>
-                    <div className="recent-entry">
-                        <img src={assets.message_icon} alt="" />
-                        <p>What is React ...</p>
-                    </div>
+                    {prevPrompt.map((item,index) => {
+                        return (
+                            <div onClick={()=>loadPrompt(item)} className="recent-entry">
+                                <img src={assets.message_icon} alt="" />
+                                <p>{item.slice(0,18)} ...</p>
+                            </div>
+                        )
+                    })}
                 </div> : null}
             </div>
             <div className="bottom">
                 <div className="bottom-item recent-entry">
                     <img src={assets.question_icon} alt="" />
-                    {extended?<p>Help</p>:null}
+                    {extended ? <p>Help</p> : null}
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.history_icon} alt="" />
-                    {extended?<p>Activity</p>:null}
+                    {extended ? <p>Activity</p> : null}
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.setting_icon} alt="" />
-                    {extended?<p>Settings</p>:null}
+                    {extended ? <p>Settings</p> : null}
                 </div>
             </div>
         </div>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,50 +1,3 @@
-// import { createContext, useState } from "react";
-// import run from "../config/gemini";
-
-// export const Context = createContext();
-
-// const ContextProvider = (props) =>{
-
-//    const [input,setInput] = useState("");
-//    const [recentPrompt,setRecentPrompt] = useState("");
-//    const [prevPrompt,setPrevPrompt]=  useState([]);
-//    const [showResult,setShowResult] = useState(false);
-//    const [loading,setLoading] = useState(false);
-//    const [resultData,setResultData]= useState("");
-
-
-//     const onSent = async (prompt) =>{
-//       setResultData("");
-//       setLoading(true);
-//       setShowResult(true);
-//       const response =  await run(input);
-//       setResultData(response);
-//       setLoading(false);
-//       setInput("")
-
-//     }
-
-//     const contextValue = {
-//       prevPrompt,
-//       setPrevPrompt,
-//       onSent,
-//       setRecentPrompt,
-//       recentPrompt,
-//       showResult,
-//       loading,
-//       resultData,
-//       input,
-//       setInput
-//     }
-
-//     return(
-//        <Context.Provider value={contextValue}>
-//         {props.children}
-//        </Context.Provider>
-//     )
-// } 
-// export default ContextProvider
-
 import { createContext, useState } from "react";
 import run from "../config/gemini";
 
@@ -57,17 +10,55 @@ const ContextProvider = (props) => {
   const [showResult, setShowResult] = useState(false);
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState("");
-
+  // for delay function 
+  const delayPara = (index, nextWord) => {
+    setTimeout(function (){
+      setResultData(prev=>prev+nextWord);
+    },75*index)
+  }
+  const newChat = () =>{
+    setLoading(false);
+    setShowResult(false);
+  }
   const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    let response;
+    if(prompt !== undefined){
+      response = await run(prompt);
+      setRecentPrompt(prompt);
+    }
+    else{
+      setPrevPrompt(prev=>[...prev,input]);
+      setRecentPrompt(input);
+      response = await run(input)
+    }
 
-   setResultData("");
-   setLoading(true);
-   setShowResult(true);
-   setRecentPrompt(input);
-   const response = await run(prompt);
-   setResultData(response);
-   setLoading(false);
-   setInput("")
+    setRecentPrompt(input);
+    setPrevPrompt(prev=>[...prev,input])
+
+    let responseArray = response.split("**");
+    let newAResponse="";
+
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newAResponse += responseArray[i];
+      }
+      else {
+        newAResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+
+    let newResponse2 = newAResponse.split("*").join("</br>");
+    let newResponseArray = newResponse2.split(" ");
+    for(let i= 0 ;i<newResponseArray.length;i++)
+      {
+        const nextWord = newResponseArray[i];
+        delayPara(i,nextWord+" ");
+      }
+    setLoading(false);
+    setInput("");
   };
 
   const contextValue = {
@@ -81,8 +72,8 @@ const ContextProvider = (props) => {
     resultData,
     input,
     setInput,
+    newChat,
   };
-
   return <Context.Provider value={contextValue}>{props.children}</Context.Provider>;
 };
 
